c3881fe86b4856403400138fca7f4f9271872737
Vitalii Kulyk <vitcool@ukr.net>
2018-11-14 08:59:15 +0200

    [FC-1] Add doctor function is added

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,32 @@
 const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+const _ = require('lodash');
+
+admin.initializeApp();
 
 exports.helloWorld = functions.https.onRequest((request, response) => {
- response.send("Hello from Firebase!");
+  response.send('Hello from Firebase!');
 });
 
+exports.addDoctor = functions.https.onCall((data, context) => {
+    const {email} = data;
+    return grantTeacherRole(email).then((result)=> {
+        return {
+            result: `Request is fulfilled ${email} is now a doctor!`
+        }
+    })
+})
+
+async function grantTeacherRole(email) {
+  const user = await admin.auth().getUserByEmail(email);
+  //custom claims check
+  if (user.customClaims && _.isEmpty(user.customClaims)) {
+    return;
+  }
+  return admin.auth.setCustomUserClaims(user.uid, {
+    doctor: true
+  });
+}
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
